aca6426cf5956a33d0696c2490eadbae2fd51532
Lukas Kliukas <lukaskliukas@gmail.com>
2022-06-12 20:55:41 +0300

    Header component edited

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import css from './Header.module.css';
 import Container from '../UI/Container/Container';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return <Container className={css.container}>Header</Container>;
+  return (
+    <Container className={css.container}>
+      <img
+        className={css.logo}
+        src='images/covid-19-logo.jpg'
+        alt='covid19-logo'
+      />
+      <nav>
+        <Link className={css.link} to='/'>
+          Home
+        </Link>
+      </nav>
+    </Container>
+  );
 };
 
 export default Header;
