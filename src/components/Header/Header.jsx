0bf601a5274a77b5f231d03d3730080b5c010cd0
Lukas Kliukas <lukaskliukas@gmail.com>
2022-06-12 19:36:01 +0300

    Header component created

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import css from './Header.module.css';
+import Container from '../UI/Container/Container';
+
+const Header = () => {
+  return <Container className={css.container}>Header</Container>;
+};
+
+export default Header;
